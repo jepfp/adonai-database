330e17c02d88b98ccbd022cba8b381d5d6b8828b
Philipp Jenni <philippjenni@bluemail.ch>
2015-02-17 07:09:37 +0100

    Revert "https://trello.com/c/8o9cYdpl File in Datenbank speichern und laden via Link, der bereitgestellt wird. - Prüfung auf id-Parameter entfernt, da auch ein Filter gesetzt sein könnte ohne ID"
    This reverts commit e0ff71cd03dc4965e9d1bca1a1b00fae9dba97c6.

--- src/Scotty/restinterface/dao/FileDAO.php
+++ src/Scotty/restinterface/dao/FileDAO.php
@@ -3,5 +3,17 @@ namespace Scotty\restinterface\dao;
 
 class FileDAO extends AbstractDAO
 {
+
+    protected function onBeforeExecuteRead()
+    {
+        $this->throwExceptionIfNoId();
+    }
+
+    private function throwExceptionIfNoId()
+    {
+        if (! $this->request->id > 0) {
+            throw new \RuntimeException("File does not support fetching all entries. Please filter by id.");
+        }
+    }
 }
 
