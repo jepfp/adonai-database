efc3631829f86dfbfbcd76791f90cee70356dc8f
Philipp Jenni <philippjenni@bluemail.ch>
2015-04-06 19:45:42 +0200

    https://trello.com/c/H1UE6CKy FAQ erstellen mit häufigen Fragen und Beschlüssen.

---
+++ faq.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>FAQ - Liederdatenbank</title>
+<style type="text/css">
+* {
+	font-family: helvetica, arial, verdana, sans-serif;
+	font-size: 13px;
+}
+
+body {
+	background-color: #F5F5F5;
+}
+
+.question {
+	font-weight: bold;
+}
+
+.page {
+	background: white;
+	width: 21cm;
+	display: block;
+	margin: 0 auto;
+	margin-bottom: 0.5cm;
+	box-shadow: 0 0 0.5cm rgba(0, 0, 0, 0.5);
+	padding: 2cm
+}
+
+h1 {
+	font-size: 50px;
+	font-weight: 300;
+	color: #157fcc;
+	margin: 0px;
+}
+
+h2 {
+	font-weight: 300;
+	color: #157fcc;
+	font-size: 15px;
+}
+</style>
+</head>
+<body>
+	<div class="page">
+		<img src="resources/images/faq.png"
+			style="float: right; height: 55px; padding-right: 5px;" />
+		<h1>FAQ - Liederdatenbank</h1>
+		<h2>Liederbücher & Liednummern</h2>
+		<p class="question">Wieso gibt es mehrere Liederbücher?</p>
+		<p>In der Datenbank ist jedes Lied einmal gespeichert. Allerdings
+			existieren viele Lieder in diversen Liederbüchern mit
+			unterschiedlichen Nummern. Für jedes Lied kannst du also angeben,
+			unter welcher Nummer man es im entsprechenden Liederbuch findet. Im
+			Beamer-Programm kannst du dann einstellen, mit welchem Liederbuch du
+			arbeiten möchtest.</p>
+		<p class="question">Wie viele Liederbücher gibt es?</p>
+		<p>Die Anzahl ist unbeschränkt. Falls du ein weiteres Liederbuch mit
+			deinen eigenen Nummern hinzufügen möchtest, schreib uns kurz an
+			lieder@adoray.ch.</p>
+		<p class="question">Soll ich der Nummer eine 0 voranstellen (z. B.
+			045, 046)?</p>
+		<p>
+			Bitte verzichte auf diese Art der Nummerierung und füge keine
+			vorausgehenden Nullen ein. Wenn du das machst, finden andere das Lied
+			evtl. später nicht mehr, weil für das Liederprogramm die Nummern 46,
+			046 und 0046 alles unterschiedliche Nummern sind.<br>Es ist geplant,
+			in einer neuen Version des Liederprogramms alle vorausgehenden Nullen
+			automatisch zu löschen. Dabei wird dann auch darauf geachtet, dass
+			die Sortierung trotzdem noch korrekt funktioniert.
+		</p>
+		<h2>Liedtexte und Refrains</h2>
+		<p class="question">Wie viele Zeichen haben in einem Refrain oder
+			einer Strophe Platz?</p>
+		<p>Pro Zeile haben ca. 30 - 50 Zeichen Platz. Wenn du mehr Zeichen auf
+			einer Zeile hast, wird das Beamer-Programm die Schrift verkleinern.
+			Versuche also eher weniger Zeichen zu schreiben.</p>
+		<p class="question">Wie viele Zeilen haben in einem Refrain oder einer
+			Strophe Platz?</p>
+		<p>Das Beamer-Programm versucht so viele Zeilen wie möglich
+			darzustellen. Ca. 6 Zeilen haben gut Platz. Falls du mehr Zeilen
+			hinzufügst, wirst du scrollen müssen. Versuche das zu verhindern.</p>
+		<h2>Mehrsprachigkeit</h2>
+		<p class="question">Was mache ich, wenn ich zur englischen
+			Originalversion den deutschen Text hinzufügen möchte?</p>
+		<p>Füge einfach die Liedtexte in den verschiedenen Sprachen hinzu.
+			Erstelle nicht mehrere einzelne Lieder in den verschiedenen Sprachen,
+			sonst findest du die Lieder nicht mehr.</p>
+		<p class="question">Ich habe ein mehrsprachiges Lied. Wie soll ich den
+			Titel benennen?</p>
+		<p>
+			Es macht Sinn, wenn du zuerst den Originaltitel einfügst und dann,
+			getrennt mit einem Schrägstrich /, die Titel in den weiteren
+			Sprachen.<br> Beispiel: Revelation Song / Heilig, heilig, das Lamm
+			Gottes
+		</p>
+		<h2>Noten</h2>
+		<p class="question">Kann ich die Noten zu einem Lied herunterladen?</p>
+		<p>Ja, zu vielen Liedern existieren die Noten in Form eines PDFs.
+			Dieses kannst du herunterladen, wenn es verfügbar ist. Ob ein PDF zur
+			Verfügung steht, siehst du, wenn du das Lied öffnest.</p>
+		<p class="question">Kann ich selbst Noten als PDF zu einem Lied
+			hinzufügen?</p>
+		<p>
+			Ja. Im Moment aber noch über einen Umweg: Füge das Lied mit der
+			entsprechenden Nummerierung in der Dropbox (wie bisher) hinzu. Einige
+			Tage später werden wir es dann ebenfalls zur Datenbank hinzufügen und
+			in der Dropbox wieder entfernen. Falls du Zugang zur Dropbox
+			benötigst, melde dich bei lieder@adoray.ch.<br>Hinweis: Es ist
+			geplant, das du bald selbst direkt online PDFs selbst hinzufügen
+			kannst.
+		</p>
+		<p class="question">Gibt es mehrere PDF-Dateien pro Lied?</p>
+		<p>Nein. Pro Lied kann eine PDF-Datei mit mehreren Seiten hinzugefügt
+			werden. Damit das Programm so einfach wie möglich bleibt, werden
+			mehrere Dateien pro Lied nicht unterstützt. Wenn du zwei Varianten
+			von einem Lied hast (z. B. verschiedene Varianten der Melodie oder
+			zwei verschiedene Tonarten) musst du das Lied ein zweites Mal
+			hinzufügen. Den Unterschied des einen Liedes vom anderen kannst du
+			mit einem kurzen Hinweis im Titel vermerken.</p>
+		<h2>AdorayClient</h2>
+		<p class="question">Was ist 'AdorayClient'?</p>
+		<p>AdorayClient ist das Beamer-Programm, mit dem du die Liedtexte
+			anzeigen kannst.</p>
+		<p class="question">Wo läuft AdorayClient?</p>
+		<p>AdorayClient läuft mit dem Betriebssystem Windows. Eine Portierung
+			auf Mac oder Linux ist nicht geplant. Falls du Software Entwickler
+			bist und eine entsprechende Version schreiben möchtest, darfst du
+			dich gerne bei uns melden.</p>
+		<p class="question">Kann ich Liedtexte direkt im AdorayClient
+			bearbeiten?</p>
+		<p>Nein. Die Liedtexte musst du immer online bearbeiten. So erhalten
+			alle anderen auch deine Aktualisierungen und Korrekturen.</p>
+	</div>
+</body>
+</html>
+
