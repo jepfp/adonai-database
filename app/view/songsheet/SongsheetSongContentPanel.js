3e3974da62cca13e93434ee08e3aee3a58cf5971
Philipp Jenni <philippjenni@bluemail.ch>
2015-06-20 21:22:24 +0200

    https://trello.com/c/MXtzpvM6 PDFs, die bereits hinzugefügt wurden, löschen - Löschen via GUI implementiert

--- app/view/songsheet/SongsheetSongContentPanel.js
+++ app/view/songsheet/SongsheetSongContentPanel.js
@@ -10,23 +10,46 @@ Ext.define('Songserver.view.songsheet.SongsheetSongContentPanel', {
 	align : 'stretch'
     },
 
+    dockedItems : {
+	xtype : 'toolbar',
+	dock : 'bottom',
+	style : {
+	    backgroundColor : '#f5f5f5'
+	},
+	items : [ {
+	    itemId : 'removeSongsheet',
+	    xtype : 'button',
+	    hidden : true,
+	    icon : 'resources/images/silk/icons/page_delete.png',
+	    text : 'Noten löschen',
+	    listeners : {
+		click : function(button, e) {
+		    this.up('songserver-songsheetSongContentPanel').deleteSongsheet(true);
+		}
+	    }
+	} ]
+    },
     createAndAddSongsheetThumbnailPanels : function() {
-	var songPanel = this.up("songserver-songPanel");
-	if (songPanel.fileStore.count() > 0) {
-	    var id = songPanel.fileStore.getAt(0).get("id");
+	var songsheet = this.getFirstSongsheetItem();
+	this.removeAll();
+	if (songsheet != null) {
+	    var id = songsheet.get("id");
 	    this.add(this.createPdfThumbnail(id));
+	    this.down('#removeSongsheet').setVisible(true);
 	} else {
 	    this.add(this.createNoSongsheetAvailableThumbnail());
-	    //this.add(this.createUploadThumbnail());
+	    this.down('#removeSongsheet').setVisible(false);
+	    // this.add(this.createUploadThumbnail());
 	}
     },
 
     privates : {
 	createPdfThumbnail : function(fileId) {
 	    return Ext.create('Songserver.view.songsheet.SongsheetThumbnailPanel', {
-		html : '<a href="src/ext-rest-interface.php/file?filter=[{%22property%22:%22filemetadata_id%22,%22value%22:' + fileId + '}]" target="_blank"><div>' + //
-		'<img src="resources/images/pdf_icon.png" height="80px" />' + //
-		'<br>Noten anzeigen.</div></a>'
+		html : '<a href="src/ext-rest-interface.php/file?filter=[{%22property%22:%22filemetadata_id%22,%22value%22:' + fileId
+			+ '}]" target="_blank"><div>' + //
+			'<img src="resources/images/pdf_icon.png" height="80px" />' + //
+			'<br>Noten anzeigen.</div></a>'
 	    });
 	},
 
@@ -41,6 +64,44 @@ Ext.define('Songserver.view.songsheet.SongsheetSongContentPanel', {
 	    return Ext.create('Songserver.view.songsheet.SongsheetUploadThumbnailPanel', {
 		flex : 1
 	    });
+	},
+
+	getFirstSongsheetItem : function() {
+	    var songPanel = this.up("songserver-songPanel");
+	    return songPanel.fileStore.getAt(0);
+	},
+
+	deleteSongsheet : function(promptFirst) {
+	    if (promptFirst === undefined) {
+		promptFirst = true
+	    }
+
+	    if (promptFirst) {
+		var mbox = Ext.Msg.show({
+		    title : 'Löschen bestätigen',
+		    msg : 'Möchtest du die Noten zu diesem Lied wirklich löschen? Dieser Schritt kann nicht rückgängig gemacht werden.',
+		    buttons : Ext.Msg.YESNO,
+		    icon : Ext.Msg.QUESTION,
+		    fn : function(btn, text) {
+			if (btn == 'yes') {
+			    this.deleteSongsheet(false);
+			}
+		    },
+		    scope : this
+		});
+	    } else {
+		var songsheet = this.getFirstSongsheetItem();
+		songsheet.erase({
+		    scope : this,
+		    failure : function(record, operation) {
+			Ext.Msg.alert("Fehler beim Löschen", "Die Noten konnten nicht gelöscht werden. "
+				+ "Bitte informiere den Website-Verantwortlichen über diesen Fehler.");
+		    },
+		    success : function(record, operation) {
+			this.createAndAddSongsheetThumbnailPanels();
+		    }
+		});
+	    }
 	}
     }
 
---
+++ src/Scotty/restinterface/querybuilder/delete/FileMetadataDeleteQueryBuilder.php
@@ -0,0 +1,7 @@
+<?php
+namespace Scotty\restinterface\querybuilder\delete;
+
+class FileMetadataDeleteQueryBuilder extends AbstractDeleteQueryBuilder
+{
+}
+
