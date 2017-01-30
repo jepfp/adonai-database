208863a734b8f077ca9a2d9279f0247168e5665e
Philipp Jenni <philippjenni@bluemail.ch>
2017-01-30 21:58:23 +0100

    https://trello.com/c/Mak0QoWw Spenden-Button einrichten

--- content/start.php
+++ content/start.php
@@ -1,5 +1,20 @@
 <div class="navigationContent">
-Vielen Dank, dass du mithilfst das Liederbuch zu pflegen.
+Vielen Dank, dass du mithilfst, das Liederbuch zu pflegen.
 <br><br>Hast du Fragen? Dann hilft dir unser <a href="faq.php" target="blank">FAQ</a> weiter oder du kannst uns via
 <a href="mailto:lieder@adoray.ch">lieder@adoray.ch</a> kontaktieren.
+<br>
+<br>
+
+Für den Fall, dass du unsere Software auch so toll findest wie wir und du deiner Freude unbedingt Ausdruck verleihen möchtest, haben wir unten einen Spenden-Button eingerichtet. Einfach so. Danke :-).
+<br>
+<br>
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="AYXK5LFHY34SU">
+<input type="image" src="resources/images/donate.png" border="0" name="submit" alt="Spenden mit PayPal.">
+<img alt="" border="0" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+
+
 </div>
